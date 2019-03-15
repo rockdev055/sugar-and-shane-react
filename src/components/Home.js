704ad8f1e2054f37e016780b2633645f149c0fcc
rockdev55 <steve264225@gmail.com>
2019-03-14 21:36:58 -0700

    Add link to items from Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import Button from "react-bootstrap/Button"
+import { LinkContainer } from "react-router-bootstrap"
 
 export default class Home extends Component {
   render() {
@@ -10,9 +11,11 @@ export default class Home extends Component {
             <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text">
               <h1>POSHBIN</h1>
               <h3>Keep track of your Poshmark inventory</h3>
-              <Button variant="success" size="lg">
-                Get Started
-              </Button>
+              <LinkContainer to="/items">
+                <Button variant="success" size="lg">
+                  Get Started
+                </Button>
+              </LinkContainer>
             </div>
           </div>
         </div>
